514d0a5418d77bfa12cd03c269264fc8b944719f
George Svarovsky <gsvarovsky@gmail.com>
2019-11-09 19:06:50 +0000

    Refactoring

--- src/_includes/lib/BoardView.ts
+++ src/_includes/lib/BoardView.ts
@@ -27,7 +27,6 @@ export class BoardView extends InfiniteView {
   }
 
   private sync(messages: Message[]) {
-    const bv = this;
     const selection = this.svg.selectAll('.board-message')
       .data(messages, function (this: Element, msg: Message) {
         return msg ? msg['@id'] : this.id;
@@ -43,73 +42,87 @@ export class BoardView extends InfiniteView {
       .on('input', this.withThisMessage(mv => mv.update()));
     enter.select('.board-message-close circle').on('click',
       this.withThisMessage(mv => this.board.remove(mv.msg['@id'])));
-    enter.selectAll('.board-message-add circle').on('click', this.withThisMessage(mv => {
-      const id = shortId();
-      mv.msg.linkTo.push(id);
-      // TODO: Prevent collisions
-      this.board.add({ '@id': id, text: '', x: mv.msg.x + 50, y: mv.msg.y + 100, linkTo: [] });
-      this.withThatMessage(id, mv => mv.text.node().focus());
-    })).call(d3.drag() // Set up drag-to-link behaviour
-      .container(this.svg.node())
-      .clickDistance(3) // Ensure that single-click hits click handler
-      .subject(function (this: SVGElement) {
-        const button = new GroupUI(<SVGGElement>svgParent(this)), startPos = button.position;
-        return { x: d3.event.x, y: d3.event.y, target: null, button, startPos, mv: new MessageView(this, bv) };
-      })
-      .on('start', function (this: Element) {
-        const drag = d3.event.subject;
-        d3.select(this).attr('cursor', 'none');
-        drag.mv.group.classed('linking', true);
-      })
-      .on('drag', this.withThisMessage(mv => {
-        mv.group.raise(); // So that the button drags over other messages
-        const drag = d3.event.subject, [cx, cy] = drag.button.position;
-        drag.button.position = [cx + d3.event.dx, cy + d3.event.dy];
-        // Hit-test for other messages
-        const target = this.hitTest(this.svgRect(drag.button.group.node()),
-          that => !MessageView.same(mv, that) && !mv.msg.linkTo.includes(that.msg['@id']));
-        if (!MessageView.same(drag.target, target)) {
-          target && target.box.classed('link-target', true);
-          drag.target && drag.target.box.classed('link-target', false);
-        }
-        drag.target = target;
-      }))
-      .on('end', function (this: Element) {
-        const drag = d3.event.subject;
-        d3.select(this).attr('cursor', 'alias');
-        if (drag.target) {
-          const mv = new MessageView(this, bv);
-          drag.target.box.classed('link-target', false);
-          mv.msg.linkTo.push(drag.target.msg['@id']);
-          mv.update();
-        }
-        drag.button.position = drag.startPos;
-        drag.mv.group.classed('linking', false);
-      }));
+    enter.selectAll('.board-message-add circle')
+      .on('click', this.withThisMessage(this.addNewMessage))
+      .call(d3.drag() // Set up drag-to-link behaviour
+        .container(this.svg.node())
+        .clickDistance(3) // Ensure that single-click hits click handler
+        .subject(this.withThisMessage(this.linkDragSubject))
+        .on('start', this.withThisMessage(this.linkDragStart))
+        .on('drag', this.withThisMessage(this.linkDragging))
+        .on('end', this.withThisMessage(this.linkDragEnd)));
     enter.selectAll('.board-message-move circle').call(d3.drag()
       .container(this.svg.node())
-      .on('start', function (this: Element) {
-        d3.select(this).attr('cursor', 'none');
-      })
-      .on('drag', this.withThisMessage(mv => {
-        // Do not modify the message data here, just the visual location
-        const [x, y] = mv.position;
-        mv.group.raise();
-        mv.position = [x + d3.event.dx, y + d3.event.dy];
-        mv.update();
-      }))
-      .on('end', function (this: Element) {
-        d3.select(this).attr('cursor', 'grab');
-        const mv = new MessageView(this, bv);
-        // Commit the change to the message
-        const [x, y] = mv.position;
-        mv.msg.x = x;
-        mv.msg.y = y;
-        // TODO: Notify update
-      }));
+      .on('start', this.withThisMessage(this.moveDragStart))
+      .on('drag', this.withThisMessage(this.moveDragging))
+      .on('end', this.withThisMessage(this.moveDragEnd)));
 
     // Call update for everyone, including the new folks
     selection.merge(enter).each(this.withThisMessage(mv => mv.update()));
+  }
+
+  private moveDragStart(_: MessageView, dragged: SVGElement) {
+    d3.select(dragged).attr('cursor', 'none');
+  }
+
+  private moveDragging(mv: MessageView) {
+    // Do not modify the message data here, just the visual location
+    const [x, y] = mv.position;
+    mv.group.raise();
+    mv.position = [x + d3.event.dx, y + d3.event.dy];
+    mv.update();
+  }
+
+  private moveDragEnd(mv: MessageView, dragged: SVGElement) {
+    d3.select(dragged).attr('cursor', 'grab');
+    // Commit the change to the message
+    const [x, y] = mv.position;
+    mv.msg.x = x;
+    mv.msg.y = y;
+  }
+
+  private linkDragSubject(_: MessageView, dragged: SVGElement) {
+    const button = new GroupUI(<SVGGElement>svgParent(dragged)), startPos = button.position;
+    return { x: d3.event.x, y: d3.event.y, button, startPos };
+  }
+
+  private linkDragStart(mv: MessageView, dragged: SVGElement) {
+    d3.select(dragged).attr('cursor', 'none');
+    mv.group.classed('linking', true);
+  }
+
+  private linkDragging(mv: MessageView) {
+    mv.group.raise(); // So that the button drags over other messages
+    const drag = d3.event.subject, [cx, cy] = drag.button.position;
+    drag.button.position = [cx + d3.event.dx, cy + d3.event.dy];
+    // Hit-test for other messages
+    const target = this.hitTest(this.svgRect(drag.button.group.node()),
+      that => !MessageView.same(mv, that) && !mv.msg.linkTo.includes(that.msg['@id']));
+    if (!MessageView.same(drag.target, target)) {
+      target && target.box.classed('link-target', true);
+      drag.target && drag.target.box.classed('link-target', false);
+    }
+    drag.target = target;
+  }
+
+  private linkDragEnd(mv: MessageView, dragged: SVGElement) {
+    const drag = d3.event.subject;
+    d3.select(dragged).attr('cursor', 'alias');
+    if (drag.target) {
+      drag.target.box.classed('link-target', false);
+      mv.msg.linkTo.push(drag.target.msg['@id']);
+      mv.update();
+    }
+    drag.button.position = drag.startPos;
+    mv.group.classed('linking', false);
+  }
+
+  private addNewMessage(mv: MessageView) {
+    const id = shortId();
+    mv.msg.linkTo.push(id);
+    // TODO: Prevent collisions
+    this.board.add({ '@id': id, text: '', x: mv.msg.x + 50, y: mv.msg.y + 100, linkTo: [] });
+    this.withThatMessage(id, mv => mv.text.node().focus());
   }
 
   svgRect(el: Element): Rectangle {
@@ -127,10 +140,10 @@ export class BoardView extends InfiniteView {
     return foundNode && new MessageView(<Element>foundNode, this);
   }
 
-  withThisMessage(action: (mv: MessageView) => any): (this: Element) => any {
+  withThisMessage(action: (mv: MessageView, el: SVGElement) => any): (this: Element) => any {
     const bv = this;
-    return function (this: Element) {
-      return action.call(null, new MessageView(this, bv));
+    return function (this: SVGElement) {
+      return action.call(bv, new MessageView(this, bv), this);
     }
   }
 
