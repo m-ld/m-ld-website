f379cf49918c3209a74b3889e24946c5507c281d
George Svarovsky <gsvarovsky@gmail.com>
2020-03-09 18:31:39 +0000

    Checking for existing Hello

--- src/_includes/demo.ts
+++ src/_includes/demo.ts
@@ -1,5 +1,4 @@
 import { BoardView } from './lib/BoardView';
-import { shortId } from 'm-ld/dist/util';
 import { Message } from './lib/Message';
 import { clone } from 'm-ld';
 import * as Level from 'level-js';
@@ -11,50 +10,54 @@ window.onload = function () {
     throw new Error('No domain specified');
 
   clone(Level(domain), {
-    domain, genesis: true, mqttOpts: { host: 'localhost', port: 8888, protocol: 'ws' }
-  }).then(meld => {
+    domain, mqttOpts: { host: 'localhost', port: 8888, protocol: 'ws' }
+  }).then(async meld => {
     new BoardView('#board', meld);
 
-    const ids = [shortId(), shortId(), shortId()];
-
-    meld.transact({
-      '@id': ids[0],
-      '@type': 'Message',
-      text: 'Hello!',
-      x: 200, y: 100,
-      linkTo: []
-    } as Message);
-    setTimeout(() => {
+    // Check if we've already said hello
+    const hello = await meld.get('hello').toPromise();
+    if (!hello) {
       meld.transact({
-        '@insert': [
-          {
-            '@id': ids[1],
-            '@type': 'Message',
-            text: 'This is your new collaborative message board.',
-            x: 250, y: 200,
-            linkTo: []
-          } as Message,
-          {
-            '@id': ids[0], linkTo: [{ '@id': ids[1] }]
-          } as Partial<Message>
-        ]
-      } as Update);
+        '@id': 'hello',
+        '@type': 'Message',
+        text: 'Hello!',
+        x: 200, y: 100,
+        linkTo: []
+      } as Message);
+
       setTimeout(() => {
         meld.transact({
           '@insert': [
             {
-              '@id': ids[2],
+              '@id': 'thisIs',
               '@type': 'Message',
-              text: "We'll use it to demonstrate how m-ld works.",
-              x: 300, y: 300,
+              text: 'This is your new collaborative message board.',
+              x: 250, y: 200,
               linkTo: []
             } as Message,
             {
-              '@id': ids[1], linkTo: [{ '@id': ids[2] }]
+              '@id': 'hello', linkTo: [{ '@id': 'thisIs' }]
             } as Partial<Message>
           ]
         } as Update);
+
+        setTimeout(() => {
+          meld.transact({
+            '@insert': [
+              {
+                '@id': 'weUse',
+                '@type': 'Message',
+                text: "We'll use it to demonstrate how m-ld works.",
+                x: 300, y: 300,
+                linkTo: []
+              } as Message,
+              {
+                '@id': 'thisIs', linkTo: [{ '@id': 'weUse' }]
+              } as Partial<Message>
+            ]
+          } as Update);
+        }, 2000);
       }, 2000);
-    }, 2000);
+    }
   });
 }
